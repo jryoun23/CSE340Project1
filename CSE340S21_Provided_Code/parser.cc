76ca438add0db803b269771327437b96d57f5873
jryoun23 <jryoun23@asu.edu>
2021-02-02 21:56:17 -0700

    firts commit

---
+++ CSE340S21_Provided_Code/parser.cc
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) Rida Bazzi, 2020
+ *
+ * Do not share this file with anyone
+ *
+ * Do not post this file or derivatives of
+ * of this file online
+ *
+ */
+#include <iostream>
+#include <cstdlib>
+#include "parser.h"
+
+using namespace std;
+
+void Parser::syntax_error()
+{
+    cout << "SYNTAX ERROR !!&%!!\n";
+    exit(1);
+}
+
+// this function gets a token and checks if it is
+// of the expected type. If it is, the token is
+// returned, otherwise, synatx_error() is generated
+// this function is particularly useful to match
+// terminals in a right hand side of a rule.
+// Written by Mohsen Zohrevandi
+Token Parser::expect(TokenType expected_type)
+{
+    Token t = lexer.GetToken();
+    if (t.token_type != expected_type)
+        syntax_error();
+    return t;
+}
+
+// Parsing
+
+// This function is simply to illustrate the GetToken() function
+// you will not need it for your project and you can delete it
+// the function also illustrates the use of peek()
+void Parser::ConsumeAllInput()
+{
+    Token token;
+    int i = 1;
+    
+    token = lexer.peek(i);
+    token.Print();
+    while (token.token_type != END_OF_FILE)
+    {
+        i = i+1;
+        token = lexer.peek(i);
+        token.Print();
+    }
+   
+    token = lexer.GetToken();
+    token.Print();
+    while (token.token_type != END_OF_FILE)
+    {
+        token = lexer.GetToken();
+        token.Print();
+    }
+
+}
+
+int main()
+{
+	// note: the parser class has a lexer object instantiated in it. You should not be declaring
+    // a separate lexer object. You can access the lexer object in the parser functions as shown in the
+    // example method Parser::ConsumeAllInput
+    // If you declare another lexer object, lexical analysis will not work correctly
+    Parser parser;
+
+	parser.ConsumeAllInput();
+	
+}
